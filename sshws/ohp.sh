5112cdf5fbb417c03814d5f1a513166705887119
awanklod <137737752+awanklod@users.noreply.github.com>
2025-07-29 21:52:07 +0700

    Update and rename ohp (2).sh to ohp.sh

--- sshws/ohp.sh
+++ sshws/ohp.sh
@@ -15,7 +15,7 @@ LIGHT='\033[0;37m'
 # Getting
 
 # Download File Ohp
- wget -O /usr/local/bin/ohpserver "https://v4.serverpremium.web.id:81/os/sshws/ohpserver"
+ wget -O /usr/local/bin/ohpserver "https://raw.githubusercontent.com/awanklod/os/main/sshws/ohpserver"
  chmod +x /usr/local/bin/ohpserver
 # Installing Service
 # SSH OHP Port 8181
