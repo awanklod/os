2cec4473351b3db90d6c1d151be49a73b7abe858
awanklod <137737752+awanklod@users.noreply.github.com>
2025-07-30 18:00:23 +0700

    Update insshws.sh

--- sshws/insshws.sh
+++ sshws/insshws.sh
@@ -1,23 +1,4 @@
 #!/bin/bash
-# Proxy For Edukasi & Imclass
-file_path="/etc/handeling"
-
-# Cek apakah file ada
-if [ ! -f "$file_path" ]; then
-    # Jika file tidak ada, buat file dan isi dengan dua baris
-    echo -e "bismillahirrahmanirrahim\nCyan" | sudo tee "$file_path" > /dev/null
-    echo "File '$file_path' berhasil dibuat."
-else
-    # Jika file ada, cek apakah isinya kosong
-    if [ ! -s "$file_path" ]; then
-        # Jika file ada tetapi kosong, isi dengan dua baris
-        echo -e "bismillahirrahmanirrahim\nCyan" | sudo tee "$file_path" > /dev/null
-        echo "File '$file_path' kosong dan telah diisi."
-    else
-        # Jika file ada dan berisi data, tidak lakukan apapun
-        echo "File '$file_path' sudah ada dan berisi data."
-    fi
-fi
 # Link Hosting Kalian
 sudo apt install python3
 
